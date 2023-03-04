622b703425d14469e2e6f0bb72af5cc177d22bd8
Macbook <macbook@Macbooks-MacBook-Air-2.local>
2023-03-04 09:44:03 +0100

    added timer

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Personalinfo from "../shared/Personalinfo";
 
 const Profile = () => {
   return (
@@ -6,6 +7,7 @@ const Profile = () => {
       <div className='h-[100vh]'>
         <div className='absolute top-8 text-3xl font-semibold'>Profile</div>
         <section>
+        <Personalinfo />
         </section>
       </div>
     </React.Fragment>
--- src/pages/Zones.jsx
+++ src/pages/Zones.jsx
@@ -6,14 +6,15 @@ import waterneeded from '../assets/waterneeded.svg'
 import soilmoisture from '../assets/soilmoisture.svg'
 import soil from '../assets/soil.svg'
 import mapzoneA from '../assets/mapzoneA.svg'
-
+import lastspray from '../assets/lastspray.svg'
+import nextspray from '../assets/nextspray.svg'
 const Zones = () => {
   return (
     <React.Fragment>
       <div className='h-[100vh]'>
         <div className='absolute top-8 text-3xl font-semibold'>Zones</div>
         <div className='flex gap-6 mt-6'>
-        <div className='flex flex-col gap-6 w-1/4'>
+        <div className='flex flex-col gap-6 w-1/4 '>
           <img src={zoneA} alt="" className=''/>
           <img src={zoneB} alt="" className=''/>
           <img src={zoneC} alt="" className=''/>
@@ -39,7 +40,15 @@ const Zones = () => {
               <div className='flex gap-6 w-[130px]'>
                   <img src={waterneeded} alt="" />
                   <img src={soilmoisture} alt="" />
-              </div>    
+              </div>  
+              <div className='flex flex-col gap-4'>
+                <div className='font-semibold'>Last spray: </div>
+                <img src={lastspray} alt="" className='w-3/4' />
+              </div>
+              <div className='flex flex-col gap-4'>
+                <div className='font-semibold'>Estimated next spray: </div>
+                <img src={nextspray} alt="" className='w-3/4' />
+              </div>      
             </div>
             <div className='w-2/5'>
               <img src={mapzoneA} alt="" />
