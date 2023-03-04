f8eecd9bc2135ecb0bf0019f0d4aacd4f8b26fcd
Macbook <macbook@Macbooks-MacBook-Air-2.local>
2023-03-04 10:19:45 +0100

    profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,49 @@
 import React from 'react'
-import Personalinfo from "../shared/Personalinfo";
+import admin from '../assets/admin.svg'
+import Input from '../shared/Input'
+import InputPw from '../shared/InputPw'
+import ButtonFull from '../shared/ButtonFull'
+
 
 const Profile = () => {
   return (
     <React.Fragment>
       <div className='h-[100vh]'>
-        <div className='absolute top-8 text-3xl font-semibold'>Profile</div>
-        <section>
-        <Personalinfo />
+      <div className='absolute top-8 text-3xl font-semibold'>Profile</div>
+     <section>
+            <div className="flex flex-col justify-center items-center space-x-2 mt-6">
+          {/* avatar */}
+          <div className="overflow-hidden rounded-full  bg-gray-200">
+            <img src={admin} alt="" className='h-[150px] w-[150px]'/>
+          </div>
+
+          <div className='align-center justify-center text-center pr-3 mt-2'>
+            <p className="text-[25px]">Bachir Beleamri</p>
+            <p className="text-[18px] text-gray-500">Farmer</p>
+          </div>
+        </div>
+        <div className='flex flex-col gap-4 mt-6 justify-center items-center'>
+          <div className='flex flex-col gap-2'>
+            <p className='text-sm'>Name:</p>
+            <Input value="Bachir Beleamri" />
+          </div>
+          <div className='flex flex-col gap-2'>
+            <p className='text-sm'>Email:</p>
+            <Input value="amiElBachir@gmail.com" />
+          </div>
+          <div className='flex flex-col gap-2'>
+            <p className='text-sm'>Phone:</p>
+            <Input value="123456789" />
+          </div>
+          <div className='flex flex-col gap-2'>
+            <p className='text-sm'>Password:</p>
+            <InputPw />
+          </div>
+          <div className='w-[300px]'>
+            <ButtonFull title = "Save" color = "bg-[#2367FF]" />
+
+          </div>
+        </div>
         </section>
       </div>
     </React.Fragment>
@@ -15,3 +51,6 @@ const Profile = () => {
 }
 
 export default Profile
+
+
+
--- src/shared/InputPw.jsx
+++ src/shared/InputPw.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-export default function Input(props) {
+export default function InputPw() {
   return (
     <div>
         <input
         className="border font-regular text-sm border-gray-300 rounded-md p-3 bg-transparent focus:border-[#2367FF] outline-none duration-300 ease-in-out "
-        type="text"
-        name="fname" value={props.value}
+        type="password"
+        value="12345678"  
         placeholder='Search for anything ...'
       />
     </div>
